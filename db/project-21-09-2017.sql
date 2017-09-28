3b2736deb55ef245113b986c5823bfbd2877a039
Ngan <ngantran1808@gmail.com>
2017-09-28 22:22:49 +0700

    add db

--- db/project-21-09-2017.sql
+++ db/project-21-09-2017.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 22, 2017 at 07:34 AM
+-- Generation Time: Sep 28, 2017 at 05:21 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.5.38
 
@@ -46,7 +46,67 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `id_status`, `firstname`, `lastname`, `phone`, `address`, `email`, `username`, `password`, `description`, `date_creation`, `date_modification`) VALUES
-(1, 1, 'Ngan', 'Tran', '0906918073', '55/17/6 duong Thanh My p8.quan Tan Binh', 'ngantran1808@gmail.com', 'admin', '$2y$13$YcDuhaOfS3rpzrPA1ha6qOiEasbDBktZSVVsIKTzTkTOUffrzSh.W', 'supper admin', '2017-09-21 00:00:00', '2017-09-21');
+(1, 1, 'Ngan', 'Tran', '0906918073', '55/17/6 duong Thanh My p8.quan Tan Binh', 'ngantran1808@gmail.com', 'admin', '$2y$13$YcDuhaOfS3rpzrPA1ha6qOiEasbDBktZSVVsIKTzTkTOUffrzSh.W', 'supper admin', '2017-09-21 00:00:00', '2017-09-21'),
+(4, 2, 'Ngan', 'Tran', '0906918073', '55/17/6 đường Thành Mỹ phường 8 quận Tân Bình', 'ngantran1808@gmail.com', 'ngantran', '$2y$13$VeH4.uu9PTIrB37G4WIaxO4DMVKZp0N9jUPbnmlcqlRVJSXd2t0re', 'admin ngan', '2017-09-25 17:51:49', '2017-09-25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_role`
+--
+
+CREATE TABLE `admin_role` (
+  `admin_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `admin_role`
+--
+
+INSERT INTO `admin_role` (`admin_id`, `role_id`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_status`
+--
+
+CREATE TABLE `admin_status` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` longtext COLLATE utf8_unicode_ci,
+  `date_creation` datetime NOT NULL,
+  `date_modification` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `admin_status`
+--
+
+INSERT INTO `admin_status` (`id`, `name`, `description`, `date_creation`, `date_modification`) VALUES
+(1, 'active', 'account active', '2017-09-21 00:00:00', '2017-09-21'),
+(2, 'deactive', 'accout deactive', '2017-09-22 00:00:00', '2017-09-22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `role` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `role`) VALUES
+(1, 'supper admin', 'ROLE_ADMIN');
 
 --
 -- Indexes for dumped tables
@@ -61,6 +121,28 @@ ALTER TABLE `admin`
   ADD KEY `id_status` (`id_status`);
 
 --
+-- Indexes for table `admin_role`
+--
+ALTER TABLE `admin_role`
+  ADD PRIMARY KEY (`admin_id`,`role_id`),
+  ADD KEY `IDX_7770088A642B8210` (`admin_id`),
+  ADD KEY `IDX_7770088AD60322AC` (`role_id`);
+
+--
+-- Indexes for table `admin_status`
+--
+ALTER TABLE `admin_status`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_B63E2EC757698A6A` (`role`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -68,6 +150,16 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `admin_status`
+--
+ALTER TABLE `admin_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
@@ -78,6 +170,13 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `admin`
   ADD CONSTRAINT `FK_880E0D765D37D0F1` FOREIGN KEY (`id_status`) REFERENCES `admin_status` (`id`);
+
+--
+-- Constraints for table `admin_role`
+--
+ALTER TABLE `admin_role`
+  ADD CONSTRAINT `FK_7770088A642B8210` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_7770088AD60322AC` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
